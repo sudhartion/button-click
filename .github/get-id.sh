bf0bd57442461247ba5ac68b5863d838614ee44f
Sudharshan D M <dmsudharshan@gmail.com>
2025-06-10 00:29:31 +0530

    Add ShellCheck for checking shell scripts (#14)
    Add `shfmt` too, for finding shell scripts in the repo.

--- .github/get-id.sh
+++ .github/get-id.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "$(terraform show -json | jq --raw-output '.values.root_module.resources[] | select( .name == "button-click-server" ).values.id')"
+terraform show -json | jq --raw-output '.values.root_module.resources[] | select( .name == "button-click-server" ).values.id'
